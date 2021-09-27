f4ad8e0db52966a27a07f73c7b2fd09c11beb3db
davidoh91 <davidoh72@gmail.com>
2021-09-27 22:32:14 +0900

    21.09.27

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-//Start by <npm init> and <npm install express> inside the github repository folder
+//+ Start by <npm init> and <npm install express> inside the github repository folder
 //when pulling from git hub repository, just import index.js, package.json and package-lock.json -> then simply 'npm install'
-// console.log("hello NodeJS");
+//+ Babel -> 'npm install @babel/preset-env @babel/core @babel/node --save-dev'
+//          use 'Nodemon' for automatic trigger of Babel to turn javascript to NodeJs 
+//+ change 'scripts': {'test':~} into...
+//"scripts": {
+//     "dev": "babel-node index.js"
+//   },
+//+ Nodemon -> 'npm install nodemon --save-dev
 
-const express = require('express');
 
-const app = express();
+import express from "express";
+// const express = require('express');
+// const app = express();
+
+console.log("hello NodeJS");
+
