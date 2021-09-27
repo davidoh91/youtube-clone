90fc8d6b9ec02bcd22a2579df7b6dad5586d00c8
davidoh91 <davidoh72@gmail.com>
2021-09-27 22:02:15 +0900

    21.09.27

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 //Start by <npm init> and <npm install express> inside the github repository folder
+//when pulling from git hub repository, just import index.js, package.json and package-lock.json -> then simply 'npm install'
 console.log("hello NodeJS");
