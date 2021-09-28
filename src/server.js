323b234b1bcbdb2fec830330eeed2d3f2d8c8f82
davidoh91 <davidoh72@gmail.com>
2021-09-28 21:41:01 +0900

    21.09.28

---
+++ src/server.js
@@ -0,0 +1,57 @@
+//+ Start by <npm init> and <npm install express> inside the github repository folder
+//when pulling from git hub repository, just import index.js or server.js, package.json and package-lock.json -> then simply 'npm install'
+//+ Babel -> 'npm install @babel/preset-env @babel/core @babel/node --save-dev'
+//            use 'Nodemon' for automatically trigger of Babel node to print results on the console; the result means javascript->NodeJs 
+//+ change 'scripts': {'test':~} into...
+//"scripts": {
+//     "dev": "babel-node index.js" or "babel-node src/server.js"
+//   },
+//+ Nodemon -> 'npm install nodemon --save-dev
+//              and simply run 'npm run dev'
+
+
+import express from "express";
+import { send } from "process";
+// const express = require('express');
+// const app = express();
+
+console.log("server.js has started");
+const app = express();
+const PORT = 2500;
+const STRING = 'server listening on http://localhost:2500'
+
+const handleListening = () => 
+    console.log(STRING);
+
+//First, make the node.js server listen to the given port
+app.listen(PORT, handleListening);
+
+//handling GET homepage:  use handleHome method...when someone reaches localhost:port's root / via GET
+// const handleHome = () => console.log('somebody is trying to GET HOME');
+// app.get("/", handleHome);
+
+//handling request coming into '/' and returning the response back  
+
+const handleHome = (req, res) => {
+    return res.send("This handles / Home");
+};
+const handleReq = (req, res) => {
+    console.log(req);
+    console.log('=========================================\n\n\n\n');
+    // return res.end();
+    return res.send("This handles the request received: ");
+};
+const handleRes = (req, res) => {
+    console.log(res);
+    console.log('=========================================\n\n\n\n');
+    return res.send("This sends a response back");
+};
+const handleLogin = (req, res) => {
+    return res.send("This handles login!")
+};
+
+app.get("/", handleHome);
+app.get("/req", handleReq);
+app.get("/res", handleRes);
+app.get("/login", handleLogin);
+
