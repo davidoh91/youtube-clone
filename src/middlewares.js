a01dbdb0731f2c1023d7eea1ea0018dc9cfcb87e
davidoh91 <davidoh72@gmail.com>
2021-10-27 22:44:10 +0900

    21.10.27

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,7 +2,7 @@
 // when this app sends the event to a browser, 
 // it holds parameters in "locals" section to share with view templates
 export const localsMiddleware = (req, res, next) => {
-    console.log("Session Info: ", req.session);
+    console.log("Session Info: ", req.session.id);
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.siteName = "Youtube";
     res.locals.loggedInUser = req.session.user;
