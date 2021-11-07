0bf3540f6dd72de95fca92c5c7977b6883f03a4d
davidoh91 <davidoh72@gmail.com>
2021-11-07 22:13:45 +0900

    21.11.07

---
+++ src/client/js/main.js
@@ -0,0 +1,5 @@
+const hello = async () => {
+    alert("hi this is javascript working");
+    const x = await fetch("");
+};
+hello();
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/client/js/main.js',
+    output: {
+        filename: 'main.js',
+        path: "./assets/js",
+    },
+};
