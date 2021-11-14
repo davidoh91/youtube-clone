a816762c15d73934a1879bf17b1367fa1b247608
davidoh91 <davidoh72@gmail.com>
2021-11-14 20:35:50 +0900

    21.11.14

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,7 +3,9 @@ const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
-console.log("This javascript file adds logic to watch view\n", play, mute, time, volume);
+const currentTime = document.getElementById("currentTime")
+const totalTime = document.getElementById("totalTime")
+console.log("This javascript file adds logic to watch view\n", video, play, mute, time, volumeRange, currentTime, totalTime);
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -17,10 +19,8 @@ const handlePlayClick = (e) => {
     }
     playBtn.innerText = video.paused ? "Play": "Pause";
 };
-
 const handlePause = () => (playBtn.innerText = "Play");
 const handlePlay = () => (playBtn.innerText = "Pause");
-
 const handleMuteClick = (e) => {
     if (video.muted) {
         video.muted = false;
@@ -30,7 +30,6 @@ const handleMuteClick = (e) => {
     muteBtn.innerText = video.muted ? "Unmute" : "Mute";
     volumeRange.value = video.muted ? 0 : volumeValue;
 };
-
 const handleVolumeChange = (event) => {
     const { target: { value } } = event;
     if(video.muted) {
@@ -40,7 +39,18 @@ const handleVolumeChange = (event) => {
     volumnValue = value;
     video.volume = value;
 }
+const formatTime = (time) => {
+    new Date(time * 1000).toISOString().substr(11, 8);
+}
+const handleLoadedMetadata = (event) => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+}
+const handleTimeUpdate = (event) => {
+    currentTime.innerText = formatTime(Math.floor(video.currentTime));
+}
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedMetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
