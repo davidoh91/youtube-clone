2ed5220209bb8e6b50d1d5bafe410134266b7e69
davidoh91 <davidoh72@gmail.com>
2021-11-16 22:19:39 +0900

    21.11.16

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 import { localsMiddleware } from "./middlewares";
+import apiRouter from "./routers/apiRouter";
 
 console.log(process.cwd());
 console.log("server.js has started");
@@ -45,6 +46,7 @@ app.use("/static", express.static("assets"));
 app.use('/', rootRouter);
 app.use('/videos', videoRouter);
 app.use('/users', userRouter);
+app.use("/api", apiRouter);
 
 export default app;
 
