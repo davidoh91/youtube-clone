fcf25350cc5f915e3942024f33572617cb1b739a
davidoh91 <davidoh72@gmail.com>
2021-12-02 00:03:23 +0900

    deployment 21.12.01

--- src/init.js
+++ src/init.js
@@ -6,8 +6,8 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 30000;
-const STRING = `"server listening on http://localhost:${PORT}"`;
+const PORT = process.env.PORT || 4000;
+const STRING = `server listening on http://localhost:${PORT}`;
 const handleListening = () => console.log(STRING);
 
 app.listen(PORT, handleListening);
